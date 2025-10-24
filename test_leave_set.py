b19125851d7018ea6f1f90ff31c5b34900ab4dc8
Zachary Ansell <89551318+zachansell@users.noreply.github.com>
2025-10-24 01:45:16 -0400

    bolster leave set testing

--- test_leave_set.py
+++ test_leave_set.py
@@ -8,14 +8,26 @@ import os
 def test_leave_set_loads():
     ls = LeaveSet()
     assert len(ls.values) > 0
+    keys = list(ls.keys())
+    assert len(keys) > 0
+    
+    item_keys = set(k for k, _ in ls.items())
+    for k in keys:
+        assert k in ls.values
+        assert k in item_keys
+    
     assert '?' in ls
     assert 'S' in ls
 
 def test_normalize_leave():
     ls = LeaveSet()
     assert ls.normalize_leave('xyz') == 'XYZ'
-    assert ls.normalize_leave('zyx') == 'ZYX'
+    assert ls.normalize_leave('XYZ') == 'XYZ'
+    assert ls.normalize_leave('zyx') == 'XYZ'
     assert ls.normalize_leave('x?y') == '?XY'
+    assert ls.normalize_leave('a') == 'A'
+    assert ls.normalize_leave('AiouoQ') == 'AIOOQU'
+    assert ls.normalize_leave('P?Qriu') == '?IPQRU'
 
 def test_get_value():
     ls = LeaveSet()
@@ -49,6 +61,17 @@ def test_value_ranges():
     values = [v for v in ls.values_list() if v is not None]
     assert min(values) > -50
     assert max(values) < 100
+
+def test_unique_keys():
+    ls = LeaveSet()
+    keys = list(ls.keys())
+    unique_keys = set(keys)
+    assert len(keys) == len(unique_keys), "Initial keys in LeaveSet are not unique"
+    
+def test_value_type_consistency():
+    ls = LeaveSet()
+    for value in ls.values_list():
+        assert isinstance(value, (int, float)), f"Value {value} is not of type int or float"
 
 if __name__ == "__main__":
     pytest.main([__file__, "-v"])
